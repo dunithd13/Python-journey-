b41c2457abbac37231b1db147b1984d4db1a1fd9
Dunith Desitha Ranawansha <dunithdesitha123@gmail.com>
2026-07-06 12:08:19 +0530

    Add files via upload

---
+++ Basics/variables.py
@@ -0,0 +1,14 @@
+
+# This is my first Python program
+print(" I like Pizza !!")
+print("It is really good ")
+
+#variable = a container for a value
+
+first_name= "Dunith"
+food = "Kottu"
+print(f"Hello {first_name}")
+print(f"You like {food}")
+
+is_Student= False
+print(f"Are you a student: {is_Student}")
