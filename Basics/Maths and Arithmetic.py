a57c3736f8beadcc29bcfce9fa51de02a904b17e
Dunith Desitha Ranawansha <dunithdesitha123@gmail.com>
2026-07-07 11:54:14 +0530

    Add files via upload

---
+++ Basics/Maths and Arithmetic.py
@@ -0,0 +1,53 @@
+friends= 10
+
+#friends= friends +1
+#friends +=1
+#friends= friends-2
+#friends-=2
+#friends= friends*3
+#friends *=3
+#friends= friends/2
+#friends/=2
+
+
+#friends= friends**2
+remainder= friends%4
+
+#print(remainder)
+
+# Some Maths functions
+
+x=3.14
+y=4
+z=5
+
+#result= round(x)
+
+#result= abs(y)
+#result= pow(4,3)
+#result= max(x,y,z)
+#print(result)
+
+import math
+print(math.pi)
+print(math.e)
+#x=9.1
+#result= math.sqrt(x)
+#result= math.ceil(x)
+
+#print(result)
+
+import math
+radius = float(input("enter the radius: "))
+circumference= 2* math.pi* radius
+
+#print(f"Circumference of circle is: {round(circumference,2)} cm")
+
+# formula to calculate
+
+import math
+a= float(input("Enter side A: ")
+b= float(input("Enter side B: ")
+c= math.sqrt(pow(a,2) + pow(b,2))
+
+print(f"side C= {c}")
