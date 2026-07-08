c5f29162a480c72fcf477b5dc045224b58e6a64f
Dunith Desitha Ranawansha <dunithdesitha123@gmail.com>
2026-07-08 15:19:46 +0530

    Add files via upload

---
+++ Basics/LogicalOperators.py
@@ -0,0 +1,9 @@
+# evaluate multiple conditions (or, and , not)
+
+temp=45
+is_raining= False
+
+if temp>35 or temp <0 or is_raining:
+    print("The outdoor event is cancelled ")
+else:
+    print("The outdoor event is still scheduled")
